c80ac13a55de93c70deff5b32f5a3369dd983bab
JerezM <jerez149@gmail.com>
2021-02-19 18:10:49 -0300

    breakable block prefab added, win/lose mechanic, and score

---
+++ Assets/Scripts/Bloque.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+
+public class Bloque : MonoBehaviour {
+
+    [SerializeField] Sprite[] hitSprites; //Contendra los sprites de los bloques con diferentes daños
+    [SerializeField] int points = 10;//Will contain the sprites of the blocks with different damages
+
+    Nivel nivel = null;
+
+    int timesHitted = 0;
+
+    private void Start() {
+        CountBreakableBlocks();
+    }
+
+    private void CountBreakableBlocks() {
+        nivel = FindObjectOfType<Nivel>();
+        if (tag == "Breakable")
+        {
+            nivel.UpdateBlocks();//Will increase the number of blocks in the current level
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision) {
+        if (tag == "Breakable")
+        {
+            GetHit();
+        }
+    }
+
+    
+    //Destroy the game object, and add the points to the game score
+    private void DestroyBlock() {
+        Destroy(gameObject); 
+        nivel.DestroyBlockFromLevel();
+        FindObjectOfType<GameStatus>().AddToScore(points);
+    }
+
+    private void GetHit() {
+        timesHitted++;
+        int maxHits = hitSprites.Length + 1;
+
+        if (timesHitted >= maxHits) {
+            DestroyBlock();
+            points = 10;
+        } else {
+            ChangeSprite();
+            points += 5;
+        }
+    }
+
+    private void ChangeSprite() {
+        int spriteIndex = timesHitted - 1;
+        if (hitSprites[spriteIndex] != null) {
+            GetComponent<SpriteRenderer>().sprite = hitSprites[spriteIndex];
+        } else {
+            Debug.LogError("Array index out of bounds");
+        }
+    }
+}
---
+++ Assets/Scripts/Nivel.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Nivel : MonoBehaviour
+{
+    [SerializeField] int currentBlocks = 0;
+
+    SceneLoader sceneLoader;
+    GameStatus gameStatus;
+
+    private void Start()
+    {
+        sceneLoader = FindObjectOfType<SceneLoader>();
+        gameStatus = FindObjectOfType<GameStatus>();
+    }
+
+    //Increse the number of blocks in the current level.
+    public void UpdateBlocks()
+    {
+        currentBlocks++;
+    }
+
+    //Decrease the number of blocks in the current level. 
+    //If it is no more blocks to destroy, the next scene will be loaded, 
+    //or the game will end(depends on wich level the player is playing).
+    public void DestroyBlockFromLevel() {
+        currentBlocks--;
+        if (currentBlocks <= 0) {
+
+            if (tag == "LastLevel") {
+                sceneLoader.LoadWinGame();
+                gameStatus.GameOver();
+            } else {
+                sceneLoader.LoadNextScene();
+                gameStatus.UpdateLevel();
+            }
+            
+        }
+    }
+}
