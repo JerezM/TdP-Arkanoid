34048c64f5f4866a400ff3569d1918ccb0a81d41
JerezM <jerez149@gmail.com>
2021-02-23 16:23:55 -0300

    glitch of score in the game over scened solved

--- Assets/Scripts/LoseCollider.cs
+++ Assets/Scripts/LoseCollider.cs
@@ -5,9 +5,9 @@ using UnityEngine.SceneManagement;
 
 public class LoseCollider : MonoBehaviour {
 
-    private void OnTriggerEnter2D(Collider2D collision) {
-        SceneManager.LoadScene("Game Over");
+    private void OnTriggerEnter2D(Collider2D collision) {       
         FindObjectOfType<GameStatus>().GameOver();
+        SceneManager.LoadScene("Game Over");
     }
 
 
